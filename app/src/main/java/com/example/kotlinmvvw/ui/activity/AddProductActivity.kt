501b5ecb8ee3a4cb60b9a4c8158ef5415c2d5dd8
Parash Mainali <mainaliparash@gmail.com>
2024-06-17 12:04:59 +0545

    UpdateProduct

--- app/src/main/java/com/example/kotlinmvvw/ui/activity/AddProductActivity.kt
+++ app/src/main/java/com/example/kotlinmvvw/ui/activity/AddProductActivity.kt
@@ -1,21 +1,137 @@
 package com.example.kotlinmvvw.ui.activity
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlinmvvw.R
+import com.example.kotlinmvvw.databinding.ActivityAddProductBinding
+import com.example.kotlinmvvw.model.ProductModel
+import com.example.kotlinmvvw.repository.ProductRepositoryImpl
+import com.example.kotlinmvvw.utils.LoadingUtils
+import com.example.kotlinmvvw.utils.imageUtils
+import com.example.kotlinmvvw.viewmodel.ProductViewModel
+import com.squareup.picasso.Picasso
+import java.util.UUID
 
 class AddProductActivity : AppCompatActivity() {
+
+    lateinit var loadingUtils: LoadingUtils
+
+    lateinit var addProductBinding: ActivityAddProductBinding
+
+    lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    var imageUri : Uri? = null
+
+    lateinit var imageUtils: imageUtils
+    lateinit var productViewModel: ProductViewModel
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_product)
+        addProductBinding = ActivityAddProductBinding.inflate(layoutInflater)
+        setContentView(addProductBinding.root)
+
+        loadingUtils = LoadingUtils(this)
+
+        imageUtils = imageUtils(this)
+        imageUtils.registerActivity { url->
+            url.let {
+                imageUri = url
+                Picasso.get().load(url).into(addProductBinding.imageBrowse)
+            }
+        }
+
+        var repo = ProductRepositoryImpl()
+        productViewModel = ProductViewModel(repo)
+
+
+        addProductBinding.imageBrowse.setOnClickListener{
+            imageUtils.launchGallery(this)
+        }
+
+        addProductBinding.btnSave.setOnClickListener {
+            uploadImage()
+
+        }
+
+        addProductBinding.btnSave.setOnClickListener {
+            if(imageUri !=null){
+                uploadImage()
+            }else{
+                Toast.makeText(applicationContext,"Please upload image first",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+
+    fun uploadImage(){
+        loadingUtils.showLoading()
+        val imageName = UUID.randomUUID().toString()
+        imageUri?.let {
+            productViewModel.uploadImage(imageName,it){
+                    success, imageUrl ->
+                if(success){
+                    addProduct(imageUrl.toString(),imageName.toString())
+                }
+            }
+        }
+    }
+
+
+    fun addProduct(url: String,imageName: String) {
+        var name: String = addProductBinding.editProductName.text.toString()
+        var desc: String = addProductBinding.editProductDesc.text.toString()
+        var price: Int = addProductBinding.editProductPrice.text.toString().toInt()
+
+        var data = ProductModel("",name,desc,price,url,imageName)
+        productViewModel.addProduct(data){
+                success,message ->
+            if(success){
+                loadingUtils.dismiss()
+                Toast.makeText(applicationContext,message,
+                    Toast.LENGTH_LONG).show()
+                finish()
+            }else{
+                loadingUtils.dismiss()
+                Toast.makeText(applicationContext,message,
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/kotlinmvvw/ui/activity/UpdateProductActivity.kt
@@ -0,0 +1,129 @@
+package com.example.kotlinmvvw.ui.activity
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinmvvw.R
+import com.example.kotlinmvvw.databinding.ActivityUpdateProductBinding
+import com.example.kotlinmvvw.model.ProductModel
+import com.example.kotlinmvvw.repository.ProductRepositoryImpl
+import com.example.kotlinmvvw.utils.imageUtils
+import com.example.kotlinmvvw.viewmodel.ProductViewModel
+import com.squareup.picasso.Picasso
+
+class UpdateProductActivity : AppCompatActivity() {
+
+
+    lateinit var updateProductBinding: ActivityUpdateProductBinding
+    var id = ""
+    var imageName = ""
+
+
+    lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    var imageUri : Uri? = null
+
+    lateinit var productViewModel: ProductViewModel
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
+    }
+
+    fun uploadImage(){
+        imageUri?.let {
+            productViewModel.uploadImage(imageName, it){
+                    success, imageUrl ->
+                if (success){
+                    updateProduct(imageUrl.toString())
+                }
+            }
+        }
+    }
+
+    lateinit var imageUtils: imageUtils
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        updateProductBinding = ActivityUpdateProductBinding.inflate(layoutInflater)
+
+        setContentView(updateProductBinding.root)
+
+        var repo = ProductRepositoryImpl()
+        productViewModel = ProductViewModel(repo)
+
+        imageUtils = imageUtils(this)
+
+        imageUtils.registerActivity {
+            imageUri = it
+            Picasso.get().load(it).into(updateProductBinding.imageUpdate)
+        }
+
+        var product : ProductModel? = intent.getParcelableExtra("product")
+        id = product?.id.toString()
+        imageName = product?.imageName.toString()
+
+        updateProductBinding.ProductNameUpdate.setText(product?.name)
+        updateProductBinding.ProductDescUpdate.setText(product?.description)
+        updateProductBinding.ProductPriceUpdate.setText(product?.price.toString())
+
+        Picasso.get().load(product?.url).into(updateProductBinding.imageUpdate)
+
+        updateProductBinding.btnUpdate.setOnClickListener {
+
+            uploadImage()
+        }
+
+        updateProductBinding.imageUpdate.setOnClickListener {
+            imageUtils.launchGallery(this@UpdateProductActivity)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun updateProduct(url: String){
+        var updatedName : String = updateProductBinding.ProductNameUpdate.text.toString()
+        var updatedDesc : String = updateProductBinding.ProductDescUpdate.text.toString()
+        var updatedPrice : Int = updateProductBinding.ProductPriceUpdate.text.toString().toInt()
+
+        var data = mutableMapOf<String,Any>()
+        data["name"] = updatedName
+        data["description"] = updatedDesc
+        data["price"] = updatedPrice
+        data["url"] = url
+
+        productViewModel.updateProduct(id,data){
+                success, message ->
+            if (success){
+                Toast.makeText(applicationContext,message,
+                    Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(applicationContext,message,
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+}
